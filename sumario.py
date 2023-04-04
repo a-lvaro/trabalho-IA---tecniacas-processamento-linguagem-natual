d67ac909ab7f2039f72a32b9797121f7f05a9be1
a-lvaro <aaflneto@gmail.com>
2023-04-04 08:39:08 -0300

    classe sumário

--- sumario.py
+++ sumario.py
@@ -3,28 +3,29 @@ from manipularPDF import removerPontuacao, lerPDF, removerNumeroPagina
 
 
 class Sumario:
-    def __init__(self, path) -> None:
+    def __init__(self, path :str) -> None:
         pdfLido = lerPDF(path)
         self.__sumario = self.__extrairSumario(pdfLido)
 
     def getSumario(self) -> dict:
         return self.__sumario
 
-    def getPaginasTopico(self, topico: str) -> list:
+    def getPaginasTopico(self, topicoRegex: re) -> list:
         posicaoPaginas = []
         for keys, values in self.__sumario.items():
-            if topico == keys:
+            if re.match(topicoRegex, keys):
                 posicaoPaginas.append(values)
             elif len(posicaoPaginas) == 1:
                 posicaoPaginas.append(values)
                 return posicaoPaginas
+                
 
-    def __encontrarPaginaSumario(self, pdfLido):
+    def __encontrarPaginaSumario(self, pdfLido :object) -> str:
         for pagina in pdfLido.pages:
             if 'sumário' in pagina.extract_text().lower()[:30]:
                 return pagina.extract_text()
 
-    def __extrairSumario(self, pdfLido):
+    def __extrairSumario(self, pdfLido :object):
         paginaSumario = self.__encontrarPaginaSumario(pdfLido)
         paginaSumario = removerNumeroPagina(paginaSumario)
         paginaSumario = removerPontuacao(paginaSumario)
@@ -33,28 +34,29 @@ class Sumario:
         dic = {}
         aux = None
 
+        # TODO da pra colocar esse sumário no pandas, talvez facilite a vida
         for i in paginaSumario.split('\n')[1:]:
             p = i.split(' ', 1)[-1].strip()
             x = re.split(r'\s{2,}', p)
 
             if 'referências' in i.split(' ', 1)[0].lower():
-                dic['referências'] = x[-1].strip()
+                dic['referências'] = int(x[-1].strip()) - 1
+
+                dic['ultima pagina'] = len(pdfLido.pages) - 1
 
                 return dic
 
             if x[-1].strip().isdigit():
                 if aux != None:
-                    dic[aux] = x[-1].strip()
+                    dic[aux] = int(x[-1].strip()) - 1
                     aux = None
                 else:
-                    dic[x[0]] = x[-1].strip()
+                    dic[x[0]] = int(x[-1].strip()) - 1
             else:
                 aux = x[0].strip()
 
-        return dic
-
 
 # x = Sumario('ArquivosPT/DAR20052019.pdf')
-# print(x.getPaginasTopico('introdução'))
+# # print(x.getPaginasTopico('introdução'))
 # print('\n\n')
 # print(x.getSumario())
