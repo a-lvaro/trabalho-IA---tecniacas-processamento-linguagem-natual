ce59bd54693d363ce759fc635131c332fe85b891
a-lvaro <aaflneto@gmail.com>
2023-04-05 14:45:14 -0300

    teste

--- artigo.py
+++ artigo.py
@@ -11,35 +11,37 @@ class Artigo():
         self.__pdfLido = lerPDF(path)
         self.__sumario = Sumario(self.__pdfLido)
 
-        # self.__objetivo = Objetivo(self.__pdfLido, self.__sumario)
-        # self.__referencia = Referencia(self.__pdfLido, self.__sumario)
-        # self.__problema = Problema(self.__pdfLido, self.__sumario)
+        self.__objetivo = Objetivo(self.__pdfLido, self.__sumario)
+        self.__referencia = Referencia(self.__pdfLido, self.__sumario)
+        self.__problema = Problema(self.__pdfLido, self.__sumario)
         # self.__metodologia = Metodologia(self.__pdfLido, self.__sumario)
         self.__contribuicao = None
 
     def getSumario(self) -> dict:
         return self.__sumario.getSumario()
     
-    # def getObjetivo(self) -> str:
-    #     return self.__objetivo.getObjetivo()
+    def getObjetivo(self) -> str:
+        return self.__objetivo.getObjetivo()
     
-    # def getReferencia(self) -> list:
-    #     return self.__referencia.getReferencia()
+    def getReferencia(self) -> list:
+        for referencia in self.__referencia.getReferencia():
+            print(referencia)
     
-    # def getProblema(self) -> str:
-    #     return self.__problema.getProblema()
+    def getProblema(self) -> str:
+        return self.__problema.getProblema()
     
     # def getMetodologia(self) -> str:
     #     return self.__metodologia.getMetodologia()
 
 
-artigo = Artigo('Arquivos/2021_LeonardoAlmeida_MarcusViniciusBorges_tcc.pdf')
-print(artigo.getSumario())
+artigo = Artigo('ArquivosTeste/MarkCappelloFerreiradeSousaCorr19.pdf')
+# print(artigo.getSumario())
 # print('\n\n OBJETIVO')
 # print(artigo.getObjetivo())
 # print('\n\n')
-# print(artigo.getReferencia())
+# artigo.getReferencia()
 # print('\n\n PROBLEMA')
-# print(artigo.getProblema())
+# TODO consertar
+print(artigo.getProblema())
 # print('\n\n METODOLOGIA')
 # print(artigo.getMetodologia())
